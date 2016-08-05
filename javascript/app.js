5873d5ce4919b4c54f317e1405547592181cd963
Blaine Arnau <blainearnau@gmail.com>
2016-08-05 13:43:44 -0700

    Update anchor to button styling

--- javascript/app.js
+++ javascript/app.js
@@ -3,12 +3,17 @@ $.getJSON("mock/topspots.json", function(spots) {
 	
 	var html = '';
 	spots.forEach(function(spot) {
-		console.log(spot);
-		html += '<tr class="row"><td>' + spot.name + '</td><td>' + spot.description + '</td><td><a href="https://www.google.com/maps?q=' 
-		+ spot.location[0] + ", " + spot.location[1] + '">Link</a></td></tr>';
+		
+		html += '<tr class="row"><td>' + spot.name + '</td><td>' + spot.description + '</td><td><input class="btn btn-block btn-primary" type="button" value="Link" onclick="self.location=\'https://www.google.com/maps?q=' 
+		+ spot.location[0] + ", " + spot.location[1] + '\'"></button></td></tr>';
 	});
+
+	console.log(html);
+
+
 
 	$("tbody").append(html);
 
 	
 });
+
