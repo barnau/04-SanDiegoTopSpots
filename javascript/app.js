b29cc8887de98d871d4e7b7e7d6e34c76c8952e2
Blaine Arnau <blainearnau@gmail.com>
2016-08-07 17:35:24 -0700

    Add title to google map markers, add nav bar

--- javascript/app.js
+++ javascript/app.js
@@ -1,35 +1,24 @@
+var coordinates = [];
 
-var coordinates =[];
+$.getJSON("mock/sanDiegoTopSpots.json", function(spots) {
 
-$.getJSON("mock/topspots.json", function(spots) {
-	
-	
-	var html = '';
-	spots.forEach(function(spot) {
-		
-		html += '<tr class="row"><td>' + spot.name + '</td><td>' + spot.description + '</td><td><input class="btn btn-block btn-primary" type="button" value="Link" onclick="self.location=\'https://www.google.com/maps?q=' 
-		+ spot.location[0] + ", " + spot.location[1] + '\'"></button></td></tr>';
 
-		coordinates.push([spot.location[0], spot.location[1]]);
-	});
+    var html = '';
+    spots.forEach(function(spot) {
 
-	
+        html += '<tr class="row"><td>' + spot.name + '</td><td>' + spot.description + '</td><td><input class="btn btn-block btn-primary" type="button" value="Link" onclick="self.location=\'https://www.google.com/maps?q=' + spot.location[0] + ", " + spot.location[1] + '\'"></button></td></tr>';
+
+        coordinates.push([spot.location[0], spot.location[1], spot.name]);
+    });
 
 
 
-	$("tbody").append(html);
 
-	
+
+    $("tbody").append(html);
+
+
 });
-
-
-
-
-
-
-
-
-
 
 
 
@@ -37,38 +26,35 @@ var markers = [];
 
 function initialize() {
 
-	
-
-  var mapProp = {
-
-  	center: new google.maps.LatLng(32.7157, -117.1611),
-    zoom:12,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
 
 
-  };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var mapProp = {
 
-  
+        center: new google.maps.LatLng(32.7157, -117.1611),
+        zoom: 9,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
 
-  for(var i = 0; i < coordinates.length; i ++) {
-  	markers[i] = new google.maps.Marker({
-  		position: {lat: coordinates[i][0], lng: coordinates[i][1] },
-  		map: map
-  	});
-  }
+
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+
+    //create array of markers
+    for (var i = 0; i < coordinates.length; i++) {
+        markers[i] = new google.maps.Marker({
+            position: { lat: coordinates[i][0], lng: coordinates[i][1] },
+            map: map,
+            title: coordinates[i][2]
+        });
+    }
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
+//inject markers to google maps event listenders
+for (var i = 0; i < markers.length; i++) {
 
-	for(var i = 0; i < markers.length; i++) {
+    google.maps.event.addListener(markers[i], 'click', function() {
+        infowindow.open(map, marker);
+    });
 
-		google.maps.event.addListener(markers[i], 'click', function() {
-          infowindow.open(map, marker);
- 		});
-
-	}
-
-
-
-
+}
